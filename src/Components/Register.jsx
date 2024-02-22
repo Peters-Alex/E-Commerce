d0483d0353175f66a4dafcbafd344c6b34347eb1
Alex Peters <mr.peters@Alexs-MacBook-Air-2.local>
2024-02-21 19:23:10 -0600

    updates to register page

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,25 +1,34 @@
-
-const registerNewUser = () => {
-    console.log(registerNewUser)
-}
+import { useState } from "react";
 
 function Register() {
-    return(
-        <div> 
+    const [userInfo, setUserInfo] = useState({ email: "", username: "", password: "", name:{ firstname: "", lastname: "", } }); //{it doesnt match the api in full}
+
+    const eventHandler = (event) => {
+        event.preventDault();
+        console.log("review eventHandler")
+    }
+    const onUserInput = (e) => {
+        console.log(JSON.stringify(userInfo));
+        setUserInfo({...userInfo, [e.target.name]: e.target.value }); //... is my spread operator
+    };
+
+    return (
+        <div>
             <h1> Register Page </h1>
-            <form onSubmit={registerNewUser}> {/*button needs to go on the form*/}
+            <form onSubmit={eventHandler}> {/*button needs to go on the form so when it completed its submitted*/}
                 Username
-                <input type="text" placeholder="Username" /> <br></br>
+                <input type="text" placeholder="Username" name="username" value={userInfo.username} onChange={onUserInput} /> <br></br>
                 Password
-                <input type="password" placeholder="Password" /> <br></br>
+                <input name="password" type="password" placeholder="Password" value={userInfo.password} onChange={onUserInput} /> <br></br>
                 Email
-                <input type="email" placeholder="email" /> <br></br>
+                <input name="email" type="email" placeholder="Email" value={userInfo.email} onChange={onUserInput}/> <br></br>
                 First Name
-                <input type="text" placeholder="FirstName" /> <br></br>
+                <input name="firstname" type="text" placeholder="FirstName" value={userInfo.firstname} onChange={onUserInput}/> <br></br>
                 Last Name
-                <input type="test" placeholder="LastName" /> <br></br>
+                <input name="lastname" type="test" placeholder="LastName" value={userInfo.lastname} onChange={onUserInput}/> <br></br>
                 <button>Submit</button>
             </form>
         </div>
-)}
+    )
+}
 export default Register;
