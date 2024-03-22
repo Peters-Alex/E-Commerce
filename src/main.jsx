109bbbba8aa86153321dd0d6cd79eb40e9d083cc
Alex Peters <mr.peters@Alexs-MacBook-Air-2.local>
2024-03-22 11:50:38 -0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app.jsx';
+import App from './App.jsx';
 import './index.css';
 import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import  store  from './store.js';
 import { Provider } from 'react-redux';
 
-const router = createBrowserRouter([
-  {
-  path: "/",
-  element: <div> Hello World!</div>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//   path: "/",
+//   element: <div> Hello World!</div>,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
