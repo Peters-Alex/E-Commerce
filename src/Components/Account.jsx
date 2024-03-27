1fa08018a9253290c8deb3d9a147db45f6d007dc
Alex Peters <mr.peters@Alexs-MacBook-Air-2.local>
2024-03-27 18:07:46 -0500

    Account Page functional

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -6,59 +6,73 @@ import "./styles/Account.css";
 
 function Account(props) {
     const navigate = useNavigate();
+    const [errorMsg, setError] = useState(null)
 
-    const [userInfo, setUserInfo] = useState({ email: "", name:  })
-    
-    
-    const eventHandler = async (event) => {
-        event.preventDefault();
+    const [userInfo, setUserInfo] = useState({ firstName: "", lastName: "", email: "", homeAddress: "", shipping: "", city: "", state: "", zip: "", card: "", securityCode: "", message: "" });
 
-        props.setToken(null)
+
+    const onUserInput = (e) => {
+        if (errorMsg) {
+            setError(null);
+        }
+        const { name, value } = e.target;
         
-        navigate("/");
-    }; 
-    
+        setUserInfo(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
 
-    return(
-        <section>
+
+     };
+
+
+const eventHandler = async (event) => {
+    event.preventDefault();
+
+    props.setToken(null)
+
+    navigate("/");
+};
+
+
+return (
+    <div className="acountPg">
         <header> Welcome Back</header>
         {/* <NavBar /> */}
-        <div className="AccountStyle"> 
+        <div className="AccountStyle">
             <form onSubmit={eventHandler}>
-            <h2> Account Information </h2>
-            <form> 
-                <label> First Name</label>
-                <input name="first" type="text" placeholder='FirstName'  />
-                <label> Last Name </label>
-                <input name="first" type="text" value=''/><br></br>
-                <label> Email Address </label>
-                <input type="text" value=''/><br></br>
-                <label> Home Address 1</label>
-                <input type="text" value=''/>
-                <label> Shipping Address</label>
-                <input type="text" value=''/><br></br>
-                <label> City </label>
-                <input type="text" value=''/>
-                <label> State </label>
-                <input type="text" value=''/>
-                <label> Zip</label>
-                <input type="text" value=''/><br></br>
-                <label> Card Number </label>
-                <input type="text" value=''/>
-                <label> CC </label>
-                <input type="text" value=''/><br></br>
-                
-                <label htmlFor="message">message </label>
-                <input message=""/>
-            </form>
-            
-            
-            <button> Logout </button>
-            
+                <h2> Account Information </h2>
+                    First Name
+                    <input name="firstName" type="text" placeholder='First Name'value={userInfo.firstName} onChange={onUserInput} />
+                    Last Name 
+                    <input name="lastName" type="text" placeholder="Last Name"value={userInfo.lastName} onChange={onUserInput} /><br></br>
+                    Email Address 
+                    <input name="email" type="text" placeholder="Email " value={userInfo.email} onChange={onUserInput}/><br></br>
+                    Home Address
+                    <input name="homeAddress" type="text" placeholder="Home Address" value={userInfo.homeAddress} onChange={onUserInput}/>
+                    Shipping Address
+                    <input name="shipping" type="text" placeholder  ="Shipping Address" value={userInfo.shipping} onChange={onUserInput} /><br></br>
+                    City
+                    <input name="city" type="text" placeholder="City" value={userInfo.city} onChange={onUserInput} />
+                    State
+                    <input name="state" type="text" placeholder="State" value={userInfo.state} onChange={onUserInput} />
+                    Zip
+                    <input name="zip" type="text" placeholder="Zip"value={userInfo.zip} onChange={onUserInput} /><br></br>
+                    Card Number 
+                    <input name="card" type="text" placeholder="Card Number" value={userInfo.card} onChange={onUserInput}/>
+                    CC
+                    <input name="securityCode" type="text" placeholder="CC" value={userInfo.securityCode} onChange={onUserInput}/><br></br>
+
+                    Message
+                    <input name="message" type="text" placeholder="Deliver Instructions" value={userInfo.message} onChange={onUserInput}/> <br></br>
+
+
+                <button> CheckOut  </button>
+
             </form>
 
         </div>
-        </section>
-    )
+    </div>
+)
 }
-export default Account
+export default Account;
