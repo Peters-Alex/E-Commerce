90c29e5cbab9c404e215abb93115e7e7b143b7fc
Alex <alexpeters59@gmail.com>
2024-04-26 14:51:41 -0500

    changes to img location

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -39,7 +39,7 @@ function Register() {
 
     return (
         <div className="Register">
-            <img src="https://images.unsplash.com/photo-1543599538-a6c4f6cc5c05?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+            <img src="/assets/kaleb-tapp-J59wWPn09BE-unsplash.jpg"></img>
 
             {errorMsg ? <p>{errorMsg}</p> : <span />}
             <form onSubmit={eventHandler}> {/*button needs to go on the form so when it completed its submitted*/}
